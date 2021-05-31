43c7e60268962e485a3a36e3d8120393fa288bd7
Fedor Medvedev <fedmed25@yandex.ru>
2021-05-31 16:34:06 +0700

    -1 заменён на 0

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 function getRandomNumber(min, max) {
   if (isNaN(min) || isNaN(max) || min < 0 || min >= max) {
-    return -1;
+    return 0;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function getRandomFloat(min, max, exp) {
   if (isNaN(min) || isNaN(max) || isNaN(exp) || min < 0 || min >= max) {
-    return -1;
+    return 0;
   }
   return parseFloat(Math.random() * (max - min + Math.pow(10, -exp)).toFixed(exp)) + min;
 }
