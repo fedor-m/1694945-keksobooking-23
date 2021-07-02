e463bfb6e73970caa0ced381f6a0ca0043a44273
Fedor Medvedev <fedmed25@yandex.ru>
2021-07-02 20:54:00 +0700

    Убраны деструктуризация и switch

--- js/popup.js
+++ js/popup.js
@@ -3,47 +3,35 @@ const newAnnouncements = generateAnnouncements();
 const demo=newAnnouncements[0];
 const card=document.querySelector('#card').content;
 const newCard=card.cloneNode(true);
-const { avatar } = demo.author;
-const { title, address, price, type, rooms, guests, checkIn, checkOut, description, features, photos } = demo.offer;
-newCard.querySelector('.popup__avatar').setAttribute('src',avatar);
-newCard.querySelector('.popup__title').textContent = title;
-newCard.querySelector('.popup__text--address').textContent = address;
-newCard.querySelector('.popup__text--price').textContent = `${price} ₽ за ночь`;
-switch(type)
-{
-  case 'palace':
-    newCard.querySelector('.popup__type').textContent = 'Дворец';
-    break;
-  case 'flat':
-    newCard.querySelector('.popup__type').textContent = 'Квартира';
-    break;
-  case 'house':
-    newCard.querySelector('.popup__type').textContent = 'Дом';
-    break;
-  case 'bungalow':
-    newCard.querySelector('.popup__type').textContent = 'Бунгало';
-    break;
-  case 'hotel':
-    newCard.querySelector('.popup__type').textContent = 'Отель';
-    break;
-}
-newCard.querySelector('.popup__text--capacity').textContent =` ${rooms} комната(ы) для размещения ${guests} гостя(ей)`;
-newCard.querySelector('.popup__text--time').textContent = `Заезд после ${checkIn}, выезд до ${checkOut}`;
-newCard.querySelector('.popup__description').textContent = description;
+const typeToName = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalow': 'Бунгало',
+  'hotel': 'Отель',
+};
+newCard.querySelector('.popup__avatar').setAttribute('src',demo.author);
+newCard.querySelector('.popup__title').textContent = demo.offer.title;
+newCard.querySelector('.popup__text--address').textContent = demo.offer.address;
+newCard.querySelector('.popup__text--price').textContent = `${demo.offer.price} ₽ за ночь`;
+newCard.querySelector('.popup__type').textContent=typeToName[demo.offer.type];
+newCard.querySelector('.popup__text--capacity').textContent =` ${demo.offer.rooms} комната(ы) для размещения ${demo.offer.guests} гостя(ей)`;
+newCard.querySelector('.popup__text--time').textContent = `Заезд после ${demo.offer.checkIn}, выезд до ${demo.offer.checkOut}`;
+newCard.querySelector('.popup__description').textContent = demo.offer.description;
 newCard.querySelector('.popup__features').innerHTML = '';
-features.forEach((feature)=>{
+demo.offer.features.forEach((feature) => {
   const li=document.createElement('li');
   li.classList.add('popup__feature');
   li.classList.add(`popup__feature--${feature}`);
   newCard.querySelector('.popup__features').appendChild(li);
 });
 newCard.querySelector('.popup__photos').innerHTML = '';
-photos.forEach((photo)=> {
+demo.offer.photos.forEach((photo) => {
   const img=document.createElement('img');
-  img.setAttribute('src',photo);
+  img.setAttribute('src', photo);
   img.classList.add('popup__photo');
-  img.setAttribute('width',45);
-  img.setAttribute('height',40);
+  img.setAttribute('width', 45);
+  img.setAttribute('height', 40);
   img.setAttribute('alt', 'Фотография жилья');
   newCard.querySelector('.popup__photos').appendChild(img);
 });
