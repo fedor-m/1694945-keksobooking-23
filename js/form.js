db40aae7c5ea96144dd6149b208363dda6600d3d
Fedor Medvedev <fedmed25@yandex.ru>
2021-07-07 14:32:44 +0700

    Доработан код — часть вызовов записана в переменные

--- js/form.js
+++ js/form.js
@@ -6,6 +6,28 @@ const adFieldsets = adForm.querySelectorAll('fieldset');
 const inputs = Array.from(mapSelects)
   .concat(Array.from(mapCheckboxes))
   .concat(Array.from(adFieldsets));
+const typeToMinPrice = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+const roomsToCapacity = {
+  1: '1',
+  2: '2',
+  3: '3',
+  100: '0',
+};
+const valuesToDisable = {
+  1: ['2', '3', '0'],
+  2: ['3', '0'],
+  3: ['0'],
+  100: ['1', '2', '3'],
+};
+const type = adForm.querySelector('#type');
+const roomNumber = adForm.querySelector('#room_number');
+const capacity = adForm.querySelector('#capacity');
 
 function toggleFormFields(isBlocked) {
   if (isBlocked) {
@@ -14,49 +36,24 @@ function toggleFormFields(isBlocked) {
   } else {
     mapFilters.classList.remove('map-filters--disabled');
     adForm.classList.remove('ad-form--disabled');
-    adForm.querySelector('#capacity').value = 1;
-    adForm
-      .querySelector('#capacity')
-      .querySelectorAll('option')
-      .forEach((o) => {
-        o.value !== '1' ? (o.disabled = true) : '';
-      });
+    capacity.value = 1;
+    capacity.querySelectorAll('option').forEach((o) => {
+      o.value !== '1' ? (o.disabled = true) : '';
+    });
   }
   inputs.forEach((input) => {
     input.disabled = isBlocked;
   });
 }
 
-adForm.querySelector('#type').addEventListener('change', function () {
-  const typeToMinPrice = {
-    bungalow: 0,
-    flat: 1000,
-    hotel: 3000,
-    house: 5000,
-    palace: 10000,
-  };
-  adForm
-    .querySelector('#price')
-    .setAttribute('min', typeToMinPrice[this.value]);
-  adForm
-    .querySelector('#price')
-    .setAttribute('placeholder', typeToMinPrice[this.value]);
-});
+function onChangeType() {
+  const price = adForm.querySelector('#price');
+  price.setAttribute('min', typeToMinPrice[this.value]);
+  price.setAttribute('placeholder', typeToMinPrice[this.value]);
+}
+type.addEventListener('change', onChangeType);
 
-adForm.querySelector('#room_number').addEventListener('change', function () {
-  const roomsToCapacity = {
-    1: '1',
-    2: '2',
-    3: '3',
-    100: '0',
-  };
-  const valuesToDisable = {
-    1: ['2', '3', '0'],
-    2: ['3', '0'],
-    3: ['0'],
-    100: ['1', '2', '3'],
-  };
-  const capacity=adForm.querySelector('#capacity');
+function onSetCapacity() {
   const options = capacity.querySelectorAll('option');
   const optionsToDisable = valuesToDisable[this.value];
   capacity.value = roomsToCapacity[this.value];
@@ -68,7 +65,8 @@ adForm.querySelector('#room_number').addEventListener('change', function () {
       }
     }
   }
-});
+}
+roomNumber.addEventListener('change', onSetCapacity);
 
 /*adForm.addEventListener('submit', function(){}) */
 
